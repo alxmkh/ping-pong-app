0aa043be19106572ee490ee619f2091dbfc78746
alxmkh <molvinskih.a@gmail.com>
2022-03-14 14:53:45 +0300

    add python-env

--- api/api.py
+++ api/api.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter, HTTPException
 import asyncio
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 router = APIRouter()
 
 ping_pong_counter: int = 0
 lock = asyncio.Lock()
-
-file_path = '/usr/src/app/files/timestamp.db'
 
 
 @router.get('/pp-get-data-from-file', tags=['ping-pong'])
@@ -15,7 +17,7 @@ async def get_ping_counter_from_file() -> dict | Exception:
         global ping_pong_counter
         async with lock:
             ping_pong_counter += 1
-        with open(file_path, 'w', encoding='utf-8') as fr:
+        with open(os.getenv("MOUNT"), 'w', encoding='utf-8') as fr:
             fr.write(f'Ping / Pongs: {ping_pong_counter}')
         return {'pong': ping_pong_counter}
     except Exception as err:
@@ -28,6 +30,6 @@ async def get_ping_counter_from_rest() -> dict | Exception:
         global ping_pong_counter
         async with lock:
             ping_pong_counter += 1
-        return {'pong': ping_pong_counter}
+        return {"pong": ping_pong_counter}
     except Exception as err:
         raise HTTPException(status_code=400, detail=str(err))
